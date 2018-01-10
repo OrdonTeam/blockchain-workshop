812296aa886e39febd0553b9614988d366ac140d
ghostbuster91 <kghost0@gmail.com>
2018-01-10 19:41:41 +0100

    Generate java classes to src/main/java

--- regenerate-solidity.sh
+++ regenerate-solidity.sh
@@ -8,5 +8,5 @@ do
     solc solidity/$f --overwrite --bin --abi --optimize -o solidity-out
     binName="solidity-out/$(basename "$f" .sol).bin"
     abiName="solidity-out/$(basename "$f" .sol).abi"
-    web3j solidity generate "$binName" "$abiName" -o src/main/kotlin -p com.elpassion.blockchain.workshop.solidity
+    web3j solidity generate "$binName" "$abiName" -o src/main/java -p com.elpassion.blockchain.workshop.solidity
 done
