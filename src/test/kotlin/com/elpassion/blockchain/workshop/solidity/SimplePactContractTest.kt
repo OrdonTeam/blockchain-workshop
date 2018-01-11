adbd4ef41ab7ab787503cff1647c6216f52710c1
ghostbuster91 <kghost0@gmail.com>
2018-01-11 16:33:15 +0100

    Add first failing test

--- src/test/kotlin/com/elpassion/blockchain/workshop/solidity/SimplePactContractTest.kt
+++ src/test/kotlin/com/elpassion/blockchain/workshop/solidity/SimplePactContractTest.kt
@@ -8,17 +8,18 @@ import org.web3j.tx.Contract
 
 class SimplePactContractTest {
 
-    private val url = "https://rinkeby.infura.io/0ZevQ4HkUCzCVBOsYZcQ"
+    private val url = "http://localhost:8545"
     private val web3jService = HttpService(url)
     private val web3j = JsonRpc2_0Web3j(web3jService)
     private val credentials = Credentials.create("c65918c493e7f7dc268ac64d2e978ddf540d5995fda3e68680a5743dd1d8a106")
-    private val serverAddress = credentials.address
+    private val one = credentials.address
+    private val other = credentials.address
 
     @Test
     fun shouldConfirmAddedPact() {
-//        val contract = SimplePactContract.deploy(web3j, credentials, Contract.GAS_PRICE, Contract.GAS_LIMIT).send()
-//        contract.addPendingPact(serverAddress, serverAddress, serverAddress).send()
-//        contract.confirmPact(serverAddress, serverAddress, serverAddress).send()
-//        assert(contract.confirmedPacts(serverAddress, serverAddress, serverAddress).send())
+        val contract = SimplePactContract.deploy(web3j, credentials, Contract.GAS_PRICE, Contract.GAS_LIMIT).send()
+        contract.addPendingPact(one, other, "pact-id").send()
+        contract.confirmPact(one, other, "pact-id").send()
+        assert(contract.isConfirmed(one, other, "pact-id").send())
     }
 }
